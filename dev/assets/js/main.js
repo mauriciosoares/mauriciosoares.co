43d79f07b09df6823a1e58c430d57237f8307565
Mauricio Soares <m.soares2603@gmail.com>
2014-12-09 16:13:33 -0200

    shooting stuff!

--- dev/assets/js/main.js
+++ dev/assets/js/main.js
@@ -36,18 +36,19 @@ var GameState = function() {
 
 GameState.prototype.preload = function() {
   this.game.load.spritesheet('ship', 'assets/images/ship.png', 50, 47);
+  this.game.load.image('bullet', 'assets/images/bullet.png');
 };
 
 GameState.prototype.create = function() {
   this.game.stage.backgroundColor = 0x000000;
 
-  // setting constants
+  // setting ship constants
   this.ACCELERATION = 200;
   this.MAX_SPEED = 400;
   this.DRAG = 50;
   this.ROTATION_SPEED = 180;
 
-  this.ship = this.game.add.sprite(32, 32, 'ship');
+  this.ship = this.game.add.sprite(200, 200, 'ship');
   this.ship.anchor.setTo(0.5, 0.5);
 
   this.game.physics.enable(this.ship, Phaser.Physics.ARCADE);
@@ -55,6 +56,23 @@ GameState.prototype.create = function() {
   this.ship.body.maxVelocity.setTo(this.MAX_SPEED, this.MAX_SPEED);
 
   this.ship.body.drag.setTo(this.DRAG, this.DRAG);
+
+  // setting bullet configuration
+  this.SHOT_DELAY = 100;
+  this.BULLET_SPEED = 500;
+  this.NUMBER_OF_BULLETS = 20;
+
+  this.bulletPool = this.game.add.group();
+  for(var i = 0; i < this.NUMBER_OF_BULLETS; i += 1) {
+    var bullet = this.game.add.sprite(0, 0, 'bullet');
+    this.bulletPool.add(bullet);
+
+    bullet.anchor.setTo(- 1, 0.5);
+
+    this.game.physics.enable(bullet, Phaser.Physics.ARCADE);
+
+    bullet.kill();
+  }
 
   this.game.input.keyboard.addKeyCapture([
     Phaser.Keyboard.LEFT,
@@ -66,10 +84,12 @@ GameState.prototype.create = function() {
 
 GameState.prototype.update = function() {
   this.moveShip();
-  this.outOfBonds();
+  this.outOfBounds();
+
+  if(this.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)) this.shootBullet();
 };
 
-GameState.prototype.outOfBonds = function() {
+GameState.prototype.outOfBounds = function() {
   if(this.ship.x > this.game.width) this.ship.x = 0;
   if(this.ship.x < 0) this.ship.x = this.game.width;
   if(this.ship.y > this.game.height) this.ship.y = 0;
@@ -95,4 +115,39 @@ GameState.prototype.moveShip = function() {
 
     this.ship.frame = 0;
   }
+};
+
+GameState.prototype.shootBullet = function() {
+    // Enforce a short delay between shots by recording
+    // the time that each bullet is shot and testing if
+    // the amount of time since the last shot is more than
+    // the required delay.
+    if (this.lastBulletShotAt === undefined) this.lastBulletShotAt = 0;
+    if (this.game.time.now - this.lastBulletShotAt < this.SHOT_DELAY) return;
+    this.lastBulletShotAt = this.game.time.now;
+
+    // Get a dead bullet from the pool
+    var bullet = this.bulletPool.getFirstDead();
+
+    // If there aren't any bullets available then don't shoot
+    if (bullet === null || bullet === undefined) return;
+
+    // Revive the bullet
+    // This makes the bullet "alive"
+    bullet.revive();
+
+    // Bullets should kill themselves when they leave the world.
+    // Phaser takes care of this for me by setting this flag
+    // but you can do it yourself by killing the bullet if
+    // its x,y coordinates are outside of the world.
+    bullet.checkWorldBounds = true;
+    bullet.outOfBoundsKill = true;
+
+    // Set the bullet position to the gun position.
+    bullet.reset(this.ship.x, this.ship.y);
+    bullet.rotation = this.ship.rotation;
+
+    // Shoot it in the right direction
+    bullet.body.velocity.x = Math.cos(bullet.rotation) * this.BULLET_SPEED;
+    bullet.body.velocity.y = Math.sin(bullet.rotation) * this.BULLET_SPEED;
 };
